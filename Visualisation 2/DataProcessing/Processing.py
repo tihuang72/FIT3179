a2f9197770992d82ecaa43609a4b2aa9cb8ce784
Thomas Huang <tihuang72@gmail.com>
2024-10-06 22:54:57 +1100

    Updated stream graph almost complete

--- Visualisation 2/DataProcessing/Processing.py
+++ Visualisation 2/DataProcessing/Processing.py
@@ -3,4 +3,3 @@ data = pd.read_excel("Visualisation 2\DataProcessing\EnergyGen.xlsx",sheet_name 
 columns = data.columns
 out = pd.melt(data,id_vars = ["Fuel Type","Type"],value_vars = columns[2:],var_name="Year",value_name="Energy(GWh)")
 out.to_csv("Visualisation 2\data\EnergyGenProcc.csv",index=False)
-out.to_json("Visualisation 2\data\EnergyGenProcc.json",orient="table")
