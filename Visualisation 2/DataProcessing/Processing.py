ecd1fe3783ef349d3212c0743c2774e59eb7dbae
Thomas Huang <tihuang72@gmail.com>
2024-10-06 23:56:40 +1100

    Initial commit of map data

--- Visualisation 2/DataProcessing/Processing.py
+++ Visualisation 2/DataProcessing/Processing.py
@@ -1,4 +1,5 @@
 import pandas as pd
+#Energy Generation - stream graph
 data = pd.read_excel("Visualisation 2\DataProcessing\EnergyGen.xlsx",sheet_name = "Sheet1")
 columns = data.columns
 out = pd.melt(data,id_vars = ["Fuel Type","Type"],value_vars = columns[2:],var_name="Year",value_name="Energy(GWh)")
