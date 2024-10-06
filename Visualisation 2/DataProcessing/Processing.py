054f15f040d8af122536b0c1cbc1859023dae896
Thomas Huang <tihuang72@gmail.com>
2024-10-06 21:30:30 +1100

    Streamchart update

--- Visualisation 2/DataProcessing/Processing.py
+++ Visualisation 2/DataProcessing/Processing.py
@@ -1,5 +1,6 @@
 import pandas as pd
-data = pd.read_excel("EnergyGen.xlsx",sheet_name = "Sheet1")
+data = pd.read_excel("Visualisation 2\DataProcessing\EnergyGen.xlsx",sheet_name = "Sheet1")
 columns = data.columns
 out = pd.melt(data,id_vars = ["Fuel Type","Type"],value_vars = columns[2:],var_name="Year",value_name="Energy(GWh)")
-out.to_csv("EnergyGenProcc.csv",index=False)
+out.to_csv("Visualisation 2\data\EnergyGenProcc.csv",index=False)
+out.to_json("Visualisation 2\data\EnergyGenProcc.json",orient="index")
