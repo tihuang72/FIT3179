a867edc1c176f28a3f2fc6a9b7d3ec539cb40887
Thomas Huang <tihuang72@gmail.com>
2024-10-08 18:18:00 +1100

    Change solar data format

--- Visualisation 2/DataProcessing/Processing.py
+++ Visualisation 2/DataProcessing/Processing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 #Energy Generation - stream graph
-data = pd.read_excel("Visualisation 2\DataProcessing\EnergyGen.xlsx",sheet_name = "Sheet1")
+data = pd.read_csv("Visualisation 2\DataProcessing\LGA2016zones.csv")
 columns = data.columns
-out = pd.melt(data,id_vars = ["Fuel Type","Type"],value_vars = columns[2:],var_name="Year",value_name="Energy(GWh)")
-out.to_csv("Visualisation 2\data\EnergyGenProcc.csv",index=False)
+out = pd.melt(data,id_vars = ["LGA_CODE_2016","LGA_NAME_2016"],value_vars = columns[2:],var_name="EnergyType",value_name="Capacity")
+out.to_csv("Visualisation 2\data\SolarLGA2016.csv",index=False)
