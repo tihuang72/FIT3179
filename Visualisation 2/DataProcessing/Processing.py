d15640fc387e8f82f7bb4eedaf473aa2ccd47fdb
Thomas Huang <tihuang72@gmail.com>
2024-10-13 22:58:01 +1100

    Update Emissions Data

--- Visualisation 2/DataProcessing/Processing.py
+++ Visualisation 2/DataProcessing/Processing.py
@@ -1,6 +1,6 @@
 import pandas as pd
 #Energy Generation - stream graph
-data = pd.read_csv("Visualisation 2\DataProcessing\LGA2016zones.csv")
+data = pd.read_csv("Visualisation 2\DataProcessing\Emissions.csv")
 columns = data.columns
-out = pd.melt(data,id_vars = ["LGA_CODE_2016","LGA_NAME_2016"],value_vars = columns[2:],var_name="EnergyType",value_name="Capacity")
-out.to_csv("Visualisation 2\data\SolarLGA2016.csv",index=False)
+out = pd.melt(data,id_vars = ["Quarter"],value_vars = columns[1:],var_name="GHG",value_name="Value")
+out.to_csv("Visualisation 2\data\Emissions2.csv",index=False)
